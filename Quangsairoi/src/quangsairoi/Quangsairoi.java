3797091a07d00aa63bbecb6274f6b1a1546554ca
hainguyen1310 <155499510+hainguyen1310@users.noreply.github.com>
2024-01-19 08:43:31 +0700

    bruh

---
+++ Quangsairoi/src/quangsairoi/Quangsairoi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quangsairoi;
+
+/**
+ *
+ * @author kivil
+ */
+public class Quangsairoi {
+    public static void main(String[] args) {
+        
+        System.out.println("Quảng làm sai rồi!!!");
+    }
+    
+}
